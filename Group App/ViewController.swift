740a6f334254867e0f336a4575e29d03a4cfe620
Emily Leaver <emilyleaver@emilys-macbook-air.local>
2018-11-27 12:53:17 +0000

    Initial Commit

---
+++ Group App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Group App
+//
+//  Created by Emily Leaver on 27/11/2018.
+//  Copyright © 2018 Emily Leaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
