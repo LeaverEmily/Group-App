0bfc056cfe1ecd2e2afe84ed6fdac2a23cb85e85
Megan Woolf <s5101471@bournemouth.ac.uk>
2018-11-29 12:47:15 +0000

    added components

--- Group App/FirstGame.swift
+++ Group App/FirstGame.swift
@@ -4,5 +4,12 @@ import UIKit
 class FirstGame: UIViewController {
     
     
+    @IBAction func slider(_ sender: UISlider) {
+    }
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBAction func goButton(_ sender: UIButton) {
+    }
 }
 
