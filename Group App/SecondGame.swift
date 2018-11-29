1e30fbc89cc450f71722523544e4b98172213018
Grace Libra (s5009536) <s5009536@mac42948.student.bournemouth.ac.uk>
2018-11-29 12:44:12 +0000

    adding pods

--- Group App/SecondGame.swift
+++ Group App/SecondGame.swift
@@ -1,7 +1,27 @@
 import Foundation
 import UIKit
 
-class SecondGame: UIViewController{
+    
+    var activeGame = true
+    var playingPlayer = 1 //X
+    var placeState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    let winningCombo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
+    class SecondGame: UIViewController{
+        
+        @IBOutlet var winLabel: UILabel!
+        @IBOutlet var playBtn: UIButton!
+        @IBAction func playAgain(_ sender: AnyObject) {
+            activeGame = true
+            placeState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            playingPlayer = 1
+            for i in 1..<10 {
+                
+                if let button = view.viewWithTag(i) as? UIButton {
+                    
+                }
+            }
+        }
     
     
 }
--- Group App/ViewController.swift
+++ Group App/ViewController.swift
@@ -4,27 +4,6 @@ import MapKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var mapView: MKMapView!
-     //print "fuck"
-    
-    let locationManager = CLLocationManager()
-    let treasureManager = TreasureManager()
-    //decalarations
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-      locationManager.requestAlwaysAuthorization()
-      locationManager.delegate = self //viewcontroller now allowed to use the location manager information
-      locationManager.startUpdatingLocation()
-        
-      for treasure in treasureManager.treasures {
-            locationManager.startMonitoring(for: treasure.region)
-            mapView.addAnnotation(treasure)
-      } // add the annotation for the treasure, start monitoring user coordinates to see if they've enter a region, once in region drop a pin - specifically for when the user is in the region
-        
-    }
 
 
 }
-//sup 
