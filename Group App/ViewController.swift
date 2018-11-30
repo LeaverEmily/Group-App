781c4a6887c9152e5511a7ef53b4d96aac19701d
Emily Leaver (s5103137) <s5103137@mac38582.student.bournemouth.ac.uk>
2018-11-30 14:52:08 +0000

    Added User location
    Loads map and monitors/shows users location

--- Group App/ViewController.swift
+++ Group App/ViewController.swift
@@ -3,7 +3,31 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //to track the user location
+        mapView.userTrackingMode = .follow
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+        
+    }
+   
+
+}
+
+extension ViewController: CLLocationManagerDelegate{
+    
+}
 
 
+extension ViewController: MKMapViewDelegate{
+    
 }
