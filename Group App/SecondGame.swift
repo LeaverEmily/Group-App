93c643e9e6d157e1722fe81512f8d7b9a58620df
gracelibra <s5009536@bournemouth.ac.uk>
2018-12-07 15:36:06 +0000

    updated storyboard

--- Group App/SecondGame.swift
+++ Group App/SecondGame.swift
@@ -4,17 +4,24 @@ import UIKit
 class SecondGame: UIViewController {
     
     var activePlayer = 1 //cross
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     @IBAction func naughtButton(_ sender: UIButton) {
-        if (activePlayer == 1) {
+        
+        if (gameState[sender.tag-1] == 0)
+            
+        {   if (activePlayer == 1) {
             sender.setImage(UIImage(named: "cross.png"), for: UIControl.State())
             activePlayer = 2
         }
         else { sender.setImage(UIImage(named: "naught.png"), for: UIControl.State())
             activePlayer = 1
+      
             
         }
         
       }
     }
+
+}
 
