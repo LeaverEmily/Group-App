39a736330049eecfe8b6c5d07474828de4ad4246
Emily <leaveremily@gmail.com>
2018-12-15 17:20:56 +0000

    Started on final game
    Done storyboard and added photos to asset folder

--- Group App/ThirdGame.swift
+++ Group App/ThirdGame.swift
@@ -4,5 +4,21 @@ import UIKit
 class ThirdGame: UIViewController{
     
     
+    @IBOutlet weak var titleImage: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func correctButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func firstIncorrectButton(_ sender: Any) {
+    }
+    
+    @IBAction func secondIncorrectButton(_ sender: Any) {
+    }
+    
+    @IBAction func thirdIncorrectButton(_ sender: Any) {
+    }
+    
     
 }
