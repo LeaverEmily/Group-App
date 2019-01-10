9b7b217fd0bd60b2d258bfcee78c3b14586be4f6
Emily <leaveremily@gmail.com>
2019-01-10 11:24:46 +0000

    generating image code
    doesn't work but the error is commented out

---
+++ Group App/Guess.swift
@@ -0,0 +1,29 @@
+import Foundation
+import UIKit
+
+class Guess: NSObject {
+    
+   // let guessImage: UIImage
+    let guessImage = ["Santa.png", "ChristmasTree", "Rudolf", "Snowman"]
+    
+    
+    
+    func randomImage() -> UIImage {
+       
+    
+   // let unsignedRandomImage = arc4random_uniform(unsignedArrayImage)
+   // let randomImage = UIImage(unsignedRandomImage)
+        
+        
+       // return UIImage
+        
+    }
+    
+    
+    
+    override init(){
+       // let randomImage = guessImage.randomElement()
+    }
+    
+    
+}
